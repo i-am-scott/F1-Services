0ec65b032d7a72e03cd9682a98faad30c178332f
Scott <3310087+i-am-scott@users.noreply.github.com>
2024-11-22 09:59:32 +0000

    race result types

--- F1Models/Models/RaceType.cs
+++ F1Models/Models/RaceType.cs
@@ -11,3 +11,17 @@ internal enum RaceType
     SprintQualifying,
     SprintShootout
 }
+
+internal class RaceResult
+{
+    public const string Race = "race-result";
+    public const string Qualifying = "qualifying";
+    public const string FP1 = "practice-1";
+    public const string FP2 = "practice-2";
+    public const string FP3 = "practice-3";
+    public const string PitStopSummary = "pit-stop-summary";
+    public const string FastestLaps = "fastest-laps";
+    public const string StartingGrid = "starting-grid";
+    public const string SprintGrid = "sprint-grid";
+    public const string Sprint = "sprint-results";
+}
